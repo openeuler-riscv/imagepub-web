0811e4cc1174e70c0f90e87a8706c4708f94ee14
cph999 <cph13000@gmail.com>
2025-05-05 11:01:23 +0800

    优化新镜像页面

--- src/pages/image/index.vue
+++ src/pages/image/index.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="image-container">
     <TopBackHome />
-    <div v-if="currentVersionInfo">
-      <h2>版本信息</h2>
-      <p>版本: {{ currentVersionInfo.version }}</p>
-      <p>更新日志: {{ currentVersionInfo.changelog }}</p>
+    <div class="top-container">
+      <div class="version-and-mirror-list-container">
+        <div v-if="currentVersionInfo">
+          <h2 class="version-info-title">版本信息</h2>
+          <p class="version-info-item">版本: {{ currentVersionInfo.version }}</p>
+          <p class="version-info-item">更新日志: {{ currentVersionInfo.changelog }}</p>
+        </div>
+        <el-table v-if="mirrorList.length > 0" :data="mirrorList" style="width: 100%">
+          <el-table-column prop="url" label="镜像文件链接" min-width="150"/>
+          <el-table-column prop="tags" label="标签" min-width="60"/>
+          <el-table-column prop="hash.sha256" label="sha256" min-width="150"/>
+          <el-table-column label="操作" min-width="50">
+            <template #default="scope">
+              <el-button type="primary" @click="downloadFile(scope.row.url)" plain>下载</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
-    <el-table v-if="mirrorList.length > 0" :data="mirrorList" style="width: 100%">
-      <el-table-column prop="url" label="镜像文件链接" />
-      <el-table-column label="操作">
-        <template #default="scope">
-          <el-button type="primary" @click="downloadFile(scope.row.url)">下载</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <HelpDoc v-if="markdownURL!== ''" :markdownURL="markdownURL" :boardDetail="boardDetail" />
+    <div class="bottom-container">
+      帮助文档
+      <div class="help-container">
+        <HelpDoc v-if="markdownURL!== ''" :markdownURL="markdownURL" :boardDetail="boardDetail" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,7 +38,6 @@ import { ref, watch, onMounted } from "vue";
 import { ElButton, ElTable, ElTableColumn } from 'element-plus';
 import { useRoute } from 'vue-router';
 
-// 假设 boardStore.boardDetail 是传入的数据
 const boardStore = useBoardStore();
 const boardDetail = ref(boardStore.boardDetail);
 const markdownURL = ref('');
@@ -100,4 +110,38 @@ const downloadFile = (url) => {
 </script>
 
 <style scoped>
+.top-container {
+  padding: 20px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  .version-and-mirror-list-container{
+    width: 90%;
+    background-color: #f9f9f9;
+    border-radius: 10px;
+    padding: 10px;
+  }
+}
+
+.version-info-title {
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+}
+
+.version-info-item {
+  font-size: 1rem;
+  margin-bottom: 5px;
+}
+
+.bottom-container{
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  .help-container{
+    width: 90%;
+
+  }
+}
 </style>
