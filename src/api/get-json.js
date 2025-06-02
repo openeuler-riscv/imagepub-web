81795d921140b375e5d66957558159322b80814b
cph999 <cph13000@gmail.com>
2025-06-02 10:02:05 +0800

    首页接口更新

--- src/api/get-json.js
+++ src/api/get-json.js
@@ -1,18 +1,17 @@
 import request from "@/utils/request";
 import {useRoute} from "vue-router";
+import {getCookie} from "@/utils/cookie.js";
 
-const route = useRoute();
 
 export const getProductList = () => {
-    return request.get('/v2/zh_CN/boards.json')
+    let lang = getCookie("lang");
+    if (!lang) {
+        lang = "zh_CN"; //默认中文
+    }
+    console.log("lag",lang);
+    return request.get(`/v2/${lang}/boards.json`);
 }
 
 export const getProductVersion = () => {
     return request.get('/products/qemu/system.json')
 }
-
-export const fetchBoardDetail = async (productUri) => {
-    const response = await fetch(`/${productUri}`);
-    if (!response.ok) throw new Error(`请求失败，状态码: ${response.status}`);
-    return response.json();
-};
